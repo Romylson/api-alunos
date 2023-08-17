b8009b2353cdf6c46691105207acf59c54989928
xande <alexandre.rpff@gmail.com>
2023-08-17 11:55:49 -0300

    Revert "Primeiro commit api-alunos"
    This reverts commit 6d9ba7095796a1b43470b174c1087adbe21e09c1.

--- src/main/java/br/com/procode/api/ApiVeiculosApplication.java
+++ src/main/java/br/com/procode/api/ApiVeiculosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiAlunosApplication {
+public class ApiVeiculosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiAlunosApplication.class, args);
+		SpringApplication.run(ApiVeiculosApplication.class, args);
 	}
 
 }
