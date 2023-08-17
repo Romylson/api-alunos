fadfebc509cbe5b44d057ccf8f03fdf300e65370
Romylson <romylson98@gmail.com>
2023-08-17 12:40:36 -0300

    teste

--- src/main/java/br/com/procode/api/ApiAlunosApplication.java
+++ src/main/java/br/com/procode/api/ApiAlunosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiVeiculosApplication {
+public class ApiAlunosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiVeiculosApplication.class, args);
+		SpringApplication.run(ApiAlunosApplication.class, args);
 	}
 
 }
---
+++ src/main/java/br/com/procode/api/repository/AlunoRepository.java
@@ -0,0 +1,11 @@
+package br.com.procode.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.procode.api.entity.Aluno;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long > {
+
+}
